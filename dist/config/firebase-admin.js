b2c7768e66149fe8cc5f095d2671e3fd212a8643
serhatcancaylan54-cell <247409898+Serhat62259@users.noreply.github.com>
2025-12-04 11:02:52 +0300

    update backend code

--- dist/config/firebase-admin.js
+++ dist/config/firebase-admin.js
@@ -3,12 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
+exports.bucket = exports.db = void 0;
 const firebase_admin_1 = __importDefault(require("firebase-admin"));
-const serviceAccount_json_1 = __importDefault(require("../serviceAccount.json"));
-if (firebase_admin_1.default.apps.length === 0) {
+if (!firebase_admin_1.default.apps.length) {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
     firebase_admin_1.default.initializeApp({
-        credential: firebase_admin_1.default.credential.cert(serviceAccount_json_1.default),
+        credential: firebase_admin_1.default.credential.cert(serviceAccount),
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     });
 }
 exports.db = firebase_admin_1.default.firestore();
+exports.bucket = firebase_admin_1.default.storage().bucket();
