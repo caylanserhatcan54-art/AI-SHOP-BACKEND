c904a1b1c426f86e7e104e6be28cff7197b92de7
serhatcancaylan54-cell <247409898+Serhat62259@users.noreply.github.com>
2025-12-06 01:31:27 +0300

    cors fixed

--- src/server.ts
+++ src/server.ts
@@ -6,14 +6,23 @@ import dotenv from "dotenv";
 import productImportRouter from "./routes/productImport";
 import { publicRouter } from "./routes/public";
 import { aiRouter } from "./routes/aiRouter";
-import { qrImageRouter } from "./routes/qrImage"; // burada düzeldi
+import { qrImageRouter } from "./routes/qrImage";
 import authShopRouter from "./routes/authShop";
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+
+app.use(cors({
+  origin: [
+    "https://ai-shop-site-rk19afcy8-serhats-projects-cbfdb63c.vercel.app",
+    "https://flowai.site",
+    "http://localhost:3000"
+  ],
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders: "Content-Type,Authorization"
+}));
 
 // ROOT
 app.get("/", (req, res) => {
@@ -25,8 +34,6 @@ app.use("/products", productImportRouter);
 app.use("/api/public", publicRouter);
 app.use("/api/ai", aiRouter);
 app.use("/auth", authShopRouter);
-
-// QR IMAGE route
 app.use("/api/qr-image", qrImageRouter);
 
 // PORT
